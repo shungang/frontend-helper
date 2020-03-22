fd7a219fb996767427f1b71ffe6c05b89429e81a
gang <>
2020-03-22 16:00:10 +0800

    添加url格式化功能

--- js/init.js
+++ js/init.js
@@ -1,7 +1,39 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
+
+//获得输入框中字符长度
+String.prototype.getBytesLength = function () {
+  var str = this
+  var bytesCount = 0;
+  for (var i = 0, n = str.length; i < n; i++) {
+    var c = str.charCodeAt(i);
+    if ((c >= 0x0001 && c <= 0x007e) || (0xff60 <= c && c <= 0xff9f)) {
+      bytesCount += 1;
+    } else {
+      bytesCount += 2;
+    }
   }
-})
-// alert(app)
+  return bytesCount;
+}
+
+window.onload = function () {
+  let input = this.document.getElementById('inputStr')
+  let msg = this.document.getElementById('msg')
+  input.oninput = function () {
+    let val = input.value
+    msg.innerHTML = `共${val.length}个字符，${val.getBytesLength()}个字节`
+  }
+
+  // url 编码
+  let inputUrl = this.document.getElementById('inputUrl')
+  let outUrl = this.document.getElementById('outUrl')
+  inputUrl.oninput = function () {
+    outUrl.value = encodeURIComponent(inputUrl.value)
+  }
+
+  // url 解码
+  let deUrl = this.document.getElementById('deUrl')
+  let deOutUrl = this.document.getElementById('deOutUrl')
+  deUrl.oninput = function () {
+    deOutUrl.value = decodeURIComponent(deUrl.value)
+  }
+  
+}
--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {
+	app,
+	BrowserWindow
+} = require('electron')
 const path = require('path')
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+function createWindow() {
+	// Create the browser window.
+	const mainWindow = new BrowserWindow({
+		width: 900,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,	//允许中渲染进程中使用node
+			preload: path.join(__dirname, 'preload.js')
+		}
+	})
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+	// and load the index.html of the app.
+	mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -25,16 +29,16 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+app.on('window-all-closed', function() {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+app.on('activate', function() {
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
